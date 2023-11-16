88190d17ae0bde9e366ef0833c37af3f653797f0
Barry Barry Tee Wei Cong <barryteeweicong@student.tudelft.nl>
2023-11-16 16:50:42 +0100

    Update main.rs - Rayon Draft

--- 1-concurrency/src/main.rs
+++ 1-concurrency/src/main.rs
@@ -1,18 +1,18 @@
-use clap::Parser; // default
-use regex::bytes::Regex; // default
+use clap::Parser;
+use rayon::prelude::*;
+use regex::bytes::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::io::Read;
-use std::path::{Path, PathBuf}; // default
+use std::path::{Path, PathBuf};
 
-// The struct you need to use to print your results. // default
-pub use crate::grep_result::GrepResult; // default
+// The struct you need to use to print your results.
+pub use crate::grep_result::GrepResult;
 
-mod grep_result; // default
+mod grep_result;
 
-#[derive(Parser, Debug)] // default
-#[command(version, about)] // default
-// default struct
+#[derive(Parser, Debug)]
+#[command(version, about)]
 struct Args {
     /// The regex pattern that the user provided
     regex: String,
@@ -28,17 +28,14 @@ fn main() {
 
     // Get the paths that we should search
     let paths = if args.paths.is_empty() {
-        // If no paths were provided, we search the current path
         vec![PathBuf::from(std::env::current_dir().unwrap())]
     } else {
-        // Take all paths from the command line arguments, and map the paths to create PathBufs
         args.paths.iter().map(PathBuf::from).collect()
     };
 
-    // Above in fn main() is default. Below is to do!()
-    // Iterate through each path
-    for path in paths {
-        // Recursively walk the directory and find all files
+    // Use Rayon to parallelize the processing of paths
+    if !paths.is_empty() && rayon::current_num_threads() > 0 {
+        paths.par_iter().for_each(|path| {
         if path.is_dir() {
             if let Err(e) = process_directory(&path, &regex) {
                 eprintln!("Error processing {}: {}", path.display(), e);
@@ -48,45 +45,60 @@ fn main() {
                 eprintln!("Error processing {}: {}", path.display(), e);
             }
         }
+        });
     }
 }
 
 fn process_directory(dir_path: &Path, regex: &Regex) -> io::Result<()> {
-    for entry in std::fs::read_dir(dir_path)? {
-        let entry = entry?;
+    let entries: Vec<_> = std::fs::read_dir(dir_path)?.collect();
+
+    entries.par_iter().for_each(|entry| {
+        let entry = entry.as_ref().unwrap();
         let path = entry.path();
+
         if path.is_dir() {
-            process_directory(&path, regex)?;
+            if let Err(e) = process_directory(&path, regex) {
+                eprintln!("Error processing {}: {}", path.display(), e);
+            }
         } else {
-            process_file(&path, regex)?;
+            if let Err(e) = process_file(&path, regex) {
+                eprintln!("Error processing {}: {}", path.display(), e);
+            }
         }
-    }
+    });
+
     Ok(())
 }
+
 
 fn process_file(file_path: &Path, regex: &Regex) -> io::Result<()> {
     let file = File::open(file_path)?;
     let content: Vec<u8> = io::BufReader::new(file).bytes().collect::<Result<_, _>>()?;
 
-    for (search_ctr, line) in content.lines().enumerate() {
-        let line = String::from_utf8_lossy(line?.as_bytes()).to_string();
+    // Ensure content length and thread count are both greater than zero
+    let chunk_size = content.len() / rayon::current_num_threads().max(1);
+    if chunk_size > 0 {
+        content.par_chunks_exact(chunk_size).for_each(|chunk| {
+            for (search_ctr, line) in chunk.split(|&b| b == b'\n').enumerate() {
 
-        if regex.is_match(line.as_bytes()) {
-            let matched = regex.find(line.as_bytes()).unwrap();
-            let result = GrepResult {
-                path: file_path.to_path_buf(),
-                search_ctr,
-                line: line.to_string(),
-                matched: String::from_utf8(line.as_bytes()[matched.start()..matched.end()].to_vec())
-                    .unwrap()
-                    .to_string(),
-                ranges: vec![matched.range()],
-                content: content.clone(),
-            };
+                let line = String::from_utf8_lossy(line);
 
-            println!("{}", result);
-        }
+                if regex.is_match(line.as_bytes()) {
+                    let matched = regex.find(line.as_bytes()).unwrap();
+                    let result = GrepResult {
+                        path: file_path.to_path_buf(),
+                        search_ctr,
+                        line: line.to_string(),
+                        matched: String::from_utf8_lossy(&line.as_bytes()[matched.start()..matched.end()])
+                            .to_string(),
+                        ranges: vec![matched.range()],
+                        content: chunk.to_vec(),  // Pass a reference to the relevant part of the content
+                    };
+
+                    println!("{}", result);
+                }
+            }
+        });
     }
-
     Ok(())
 }
