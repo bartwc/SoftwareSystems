613803e1586ac751168ae4ca9e7a34aa2aebd927
Barry Barry Tee Wei Cong <barryteeweicong@student.tudelft.nl>
2023-11-17 18:16:21 +0100

    Update main.rs - Sorry previous commit is unstable. This commit is very stable for many words. However, the printing for example 3 is repeated. But the word match is very close. Example 1 and 2 is perfect with the usage of rayon. Timing of rayon is 3.111 seconds.

--- 1-concurrency/src/main.rs
+++ 1-concurrency/src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
 use rayon::prelude::*;
 use regex::bytes::Regex;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::io::Read;
+use std::fs::{self, File};
+use std::io::{self, Read};
 use std::path::{Path, PathBuf};
 
 // The struct you need to use to print your results.
@@ -36,67 +35,55 @@ fn main() {
     // Use Rayon to parallelize the processing of paths
     if !paths.is_empty() && rayon::current_num_threads() > 0 {
         paths.par_iter().for_each(|path| {
-            if path.is_dir() {
-                if let Err(e) = process_directory(&path, &regex) {
-                    eprintln!("Error processing {}: {}", path.display(), e);
-                }
-            } else {
-                if let Err(e) = process_file(&path, &regex) {
-                    eprintln!("Error processing {}: {}", path.display(), e);
-                }
-            }
+            process_path(path, &regex);
         });
     }
 }
 
-fn process_directory(dir_path: &Path, regex: &Regex) -> io::Result<()> {
-    let entries: Vec<_> = std::fs::read_dir(dir_path)?.collect();
+fn process_path(path: &Path, regex: &Regex) {
+    if path.is_dir() {
+        process_directory(path, regex);
+    } else {
+        process_file(path, regex);
+    }
+}
+
+fn process_directory(dir_path: &Path, regex: &Regex) {
+    let entries: Vec<_> = fs::read_dir(dir_path)
+        .unwrap_or_else(|e| panic!("Error reading directory {}: {}", dir_path.display(), e))
+        .collect();
 
     entries.par_iter().for_each(|entry| {
         let entry = entry.as_ref().unwrap();
         let path = entry.path();
 
         if path.is_dir() {
-            if let Err(e) = process_directory(&path, regex) {
-                eprintln!("Error processing {}: {}", path.display(), e);
-            }
+            process_directory(&path, regex);
         } else {
-            if let Err(e) = process_file(&path, regex) {
-                eprintln!("Error processing {}: {}", path.display(), e);
-            }
+            process_file(&path, regex);
         }
     });
-
-    Ok(())
 }
 
-fn process_file(file_path: &Path, regex: &Regex) -> io::Result<()> {
-    let file = File::open(file_path)?;
+fn process_file(file_path: &Path, regex: &Regex) {
+    let file = File::open(file_path)
+        .unwrap_or_else(|e| panic!("Error opening file {}: {}", file_path.display(), e));
+
     let mut content = Vec::new();
-    file.take(1024 * 1024 * 10) // Read up to 10 megabytes of content
-        .read_to_end(&mut content)?;
+    let _ = file.take(1024 * 1024 * 10).read_to_end(&mut content); // Read up to 10 megabytes of content
 
-    let content_str = String::from_utf8_lossy(&content);
+    if regex.is_match(&content) {
+        let matches: Vec<_> = regex.find_iter(&content).collect();
 
-    for (search_ctr, line) in content_str.lines().enumerate() {
-        if regex.is_match(line.as_ref()) {
-            let matches: Vec<_> = regex.find_iter(line.as_bytes()).collect();
-            if let Some(matched) = matches.get(0) {
-                let matched_str = &line[matched.start()..matched.end()];
+        let result = GrepResult {
+            path: file_path.to_path_buf(),
+            content: content.clone(),
+            ranges: matches.iter().map(|m| m.start()..m.end()).collect(),
+            search_ctr: matches.len(),
+            line: "".to_string(), // You can extract the line if needed
+            matched: "".to_string(), // You can extract the matched substring if needed
+        };
 
-                let result = GrepResult {
-                    path: file_path.to_path_buf(),
-                    search_ctr,
-                    line: String::from(line),
-                    matched: String::from(matched_str),
-                    ranges: vec![matched.range()],
-                    content: content.clone(),
-                };
-
-                println!("{}", result);
-            }
-        }
+        println!("{}", result);
     }
-
-    Ok(())
 }
