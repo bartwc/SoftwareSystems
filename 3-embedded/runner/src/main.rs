f1101818d006bae65b05df18448247b8fc853b3f
BARRY TEE WEI CONG <BarryTeeWeiCong@student.tudelft.nl>
2023-11-28 14:24:25 +0100

    Push Comments for Understanding II

--- 3-embedded/runner/src/main.rs
+++ 3-embedded/runner/src/main.rs
@@ -3,17 +3,17 @@ use std::io::{stdout, Read, Write};
 
 use tudelft_arm_qemu_runner::Runner;
 
-fn main() -> color_eyre::Result<()> {
+fn main() -> color_eyre::Result<()> { // runner is x86 laptop while embedded-code is simulated arm processor
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
 
     let binary = args().nth(1).unwrap();
-    let mut runner: Runner = Runner::new(&binary, false)?;
+    let mut runner: Runner = Runner::new(&binary, false)?; // true may look like it crash
 
     // receive up to 4 bytes
     let mut buf = [0u8; 4];
     loop {
-        let num_received = runner.stream.read(&mut buf)?;
+        let num_received = runner.stream.read(&mut buf)?; // logging purposes
         // get the portion we actually received
         let received = &buf[0..num_received];
         // print it (without newline)
