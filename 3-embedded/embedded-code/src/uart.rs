12e41bdee5afa71bb3c51c83685dd9f2cdd09a94
Zhengtao Huang <z.huang-43@student.tudelft.nl>
2023-12-05 01:23:48 +0100

    uart initialisation

--- 3-embedded/embedded-code/src/uart.rs
+++ 3-embedded/embedded-code/src/uart.rs
@@ -2,19 +2,48 @@ use core::fmt::Write;
 use tudelft_lm3s6965_pac::interrupt;
 use tudelft_lm3s6965_pac::UART0;
 
-pub struct Uart {}
+pub struct Uart {
+    uart: UART0
+}
 
 impl Uart {
     pub fn new(uart: UART0) -> Self {
-        todo!()
+        uart.ctl.write(|w|w.uart_ctl_uarten().clear_bit());
+        uart.ibrd.write(|w| unsafe {
+            w.uart_ibrd_divint().bits(10)
+        });
+        uart.fbrd.write(|w| unsafe {
+            w.uart_fbrd_divfrac().bits(54)
+        });
+        // uart.lcrh.write(|w|{
+        //     w.uart_lcrh_fen().clear_bit()
+        // });
+        uart.lcrh.write(|w| unsafe {
+            w.bits(0x00000060)
+        });
+        uart.ctl.write(|w|w.uart_ctl_uarten().set_bit());
+
+        Uart {uart}
     }
 
     pub fn write(&mut self, value: &[u8]) {
-        todo!()
+        for each in value {
+            while self.uart.fr.read().uart_fr_txff().bit_is_set() {} //busy waiting while the fifo queue is full
+            self.uart.dr.write(|w| unsafe {
+                w.uart_dr_data().bits(*each)
+            })
+        }
     }
 
     pub fn read(&mut self) -> Option<u8> {
-        todo!()
+        if self.uart.fr.read().uart_fr_rxfe().bit_is_clear() {
+            Some(self.uart.dr.read().uart_dr_data().bits())
+        }
+        else {
+            None
+        }
+
+
     }
 }
 
