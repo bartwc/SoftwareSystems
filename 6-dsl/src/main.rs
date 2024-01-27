9a5ac27d30e5134b9f7bf6b8d2c852fbff7cb2f8
Zhengtao Huang <z.huang-43@student.tudelft.nl>
2024-01-27 23:23:03 +0100

    flexible one-plane implemented

--- 6-dsl/src/main.rs
+++ 6-dsl/src/main.rs
@@ -6,6 +6,9 @@ use Mode::*;
 use Projection::*;
 
 use log::info;
+use crate::UserPreference::{EarlyOverride, HighOverride, LowOverride};
+
+const DESIGN_CHOICE: UserPreference = LowOverride;
 
 fn main() {
     // Initialize logger.
@@ -19,20 +22,19 @@ fn main() {
 /// The pedal mapping is based on the example mapping given in the DSL assignment.
 #[derive(Default)]
 struct Logic {
-    /// keep track of the selected projection
-    selected: Projection,
     selected_dose: Dose,
     selected_mode: Mode,
-    p1_on: bool,
-    p2_on: bool,
-    p3_on: bool,
-    //p4_on: bool,
-    p5_on: bool,
-    p6_on: bool,
-    // p6_on: bool,
+    high_on: bool,
+    low_on: bool,
     // you can have whatever other information that you want here
 }
 
+#[derive(PartialEq)]
+enum UserPreference{
+    HighOverride,
+    LowOverride,
+    EarlyOverride,
+}
 impl PedalMapper for Logic {
     /// We use an associated type to determine which pedal enum is used.
     /// Single-plane systems use the `ThreePedals` enum, while
@@ -42,160 +44,20 @@ impl PedalMapper for Logic {
 
     fn on_press(&self, pedal: Self::Pedals) -> Option<Request> {
         use ThreePedals::*;
-        Some(match pedal {
+        match pedal {
             // 3 pedals for low dose X-ray streaming video (one for each projection)
-            Pedal1 => Request::start(Frontal, Low, Video),
-            Pedal2 => Request::start(Lateral, Low, Video),
-            Pedal3 => Request::start(Biplane, Low, Video),
-            // 1 pedal to select the high dose projection in a round-robin fashion
-            // Pedal4 => Request::toggle_selected_projection(),
-            // // 1 pedal for high dose X-ray streaming video
-            // Pedal5 => Request::start_selected_projection(High, Video),
-            // // 1 pedal for high dose X-ray single image
-            // Pedal6 => Request::start_selected_projection(High, Image),
-        })
+            Pedal1 => Some(Request::start(Frontal, Low, Video)),
+            Pedal2 => Some(Request::start(Frontal, High, Video)),
+            Pedal3 => None
+        }
     }
 
     fn on_release(&self, pedal: Self::Pedals) -> Option<Request> {
         use ThreePedals::*;
-        Some(match pedal {
-            Pedal1 => Request::stop(Frontal, Low, Video),
-            Pedal2 => Request::stop(Lateral, Low, Video),
-            Pedal3 => Request::stop(Biplane, Low, Video),
-            // Pedal4 => return None, // nothing happens when we release pedal 3
-            // Pedal5 => Request::stop_selected_projection(High, Video),
-            // Pedal6 => Request::stop_selected_projection(High, Image),
-        })
-    }
-}
-
-impl ActionLogic<true> for Logic {
-    /// Naive implementation of request handling which does not handle
-    /// multiple pedals being pressed at once.
-    fn handle_request(&mut self, request: Request, controller: &mut Controller<true>) {
-        // This is how you can get access to the planes in case you want to inspect their status.
-        let _frontal = controller.frontal();
-        let _lateral = controller.lateral();
-
-        // Custom logging of requests.
-        info!("Processing request: {request:?}");
-
-        // In your own code (as well as the code that you generate),
-        // you should do checks before using the controller to
-        // start and stop X-rays.
-        match request {
-            Request::Start {
-                projection,
-                dose,
-                mode,
-            } => {
-                if projection == Frontal {
-                    self.p1_on = true;
-                    if self.p3_on == false && self.p2_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(projection, dose, mode);
-                    } else if self.p2_on {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Lateral, dose, mode);
-                    } else if self.p3_on {
-
-                    }
-                } else if projection == Lateral {
-                    self.p2_on = true;
-                    if self.p3_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(projection, dose, mode);
-                    }
-                }
-                if projection == Biplane {
-                    self.p3_on = true;
-                    if self.p1_on == false && self.p2_on == true {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Frontal, dose, mode);
-                    } else if self.p1_on == true && self.p2_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Lateral, dose, mode);
-                    } else if self.p1_on == false && self.p2_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Biplane, dose, mode);
-                    }
-                }
-                self.selected = projection;
-                self.selected_dose = dose;
-                self.selected_mode = mode;
-            }
-
-            Request::Stop { projection,
-                dose,
-                mode, } => {
-                if projection == Frontal {
-                    self.p1_on = false;
-                } else if projection == Lateral {
-                    self.p2_on = false;
-                } else if projection == Biplane {
-                    self.p3_on = false;
-                }
-                if self.p5_on == false && self.p6_on == false {
-                    controller.deactivate_xray();
-                }
-                if projection != Biplane && self.p3_on == true {
-                    controller.activate_xray(Biplane, dose, mode);
-                }
-                if projection != Frontal && self.p1_on == true && self.p3_on == false {
-                    self.selected = Frontal;
-                    controller.activate_xray(Frontal, dose, mode);
-                }
-                if projection != Lateral && self.p2_on == true && self.p3_on == false {
-                    self.selected = Lateral;
-                    controller.activate_xray(Lateral, dose, mode);
-                }
-            }
-
-            Request::ToggleSelectedProjection => {
-                if self.p1_on == false && self.p2_on == false && self.p3_on == false {
-                    self.selected = match self.selected {
-                        Frontal => Lateral,
-                        Lateral => Biplane,
-                        Biplane => Frontal,
-                    }
-                };
-                info!("Selected: {:?}", self.selected);
-            }
-
-            Request::StartSelectedProjection { dose, mode } => {
-                if mode == Video {
-                    self.p5_on = true;
-                } else if mode == Image {
-                    self.p6_on = true;
-                }
-                controller.deactivate_xray();
-                controller.activate_xray(self.selected, dose, mode)
-            }
-
-            Request::StopSelectedProjection { dose, mode  } => {
-                if mode == Video {
-                    self.p5_on = false;
-                } else if mode == Image {
-                    self.p6_on = false;
-                }
-
-                if self.p1_on == true || self.p2_on == true || self.p3_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, Low, Video);
-                } else if self.p1_on == false || self.p2_on == false || self.p3_on == false {
-                    controller.deactivate_xray();
-                    // controller.activate_xray(self.selected, dose, mode);
-                }
-
-                if self.p5_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, High, Video);
-                }
-                if self.p6_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, High, Image);
-                }
-            }
+        match pedal {
+            Pedal1 => Some(Request::stop(Frontal, Low, Video)),
+            Pedal2 => Some(Request::stop(Frontal, High, Video)),
+            Pedal3 => None
         }
     }
 }
@@ -205,8 +67,6 @@ impl ActionLogic<false> for Logic {
     /// multiple pedals being pressed at once.
     fn handle_request(&mut self, request: Request, controller: &mut Controller<false>) {
         // This is how you can get access to the planes in case you want to inspect their status.
-        // let _frontal = controller.frontal();
-        // let _lateral = controller.lateral();
 
         // Custom logging of requests.
         info!("Processing request: {request:?}");
@@ -220,112 +80,69 @@ impl ActionLogic<false> for Logic {
                 dose,
                 mode,
             } => {
-                if projection == Frontal {
-                    self.p1_on = true;
-                    if self.p3_on == false && self.p2_on == false {
-                        controller.deactivate_xray();
-                        //controller.activate_frontal();
-                    } else if self.p2_on {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Lateral, dose, mode);
-                    } else if self.p3_on {}
-                } else if projection == Lateral {
-                    self.p2_on = true;
-                    if self.p3_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(projection, dose, mode);
+                if dose == High {
+                    if DESIGN_CHOICE == HighOverride {
+                        self.selected_dose = dose;
+                        self.selected_mode = mode;
+                        self.high_on = true;
+                        if self.low_on == false {
+                            controller.activate_frontal(dose, mode);
+                        } else {
+                            controller.deactivate_xray();
+                            controller.activate_frontal(dose, mode);
+                        }
+                    } else if DESIGN_CHOICE == LowOverride || DESIGN_CHOICE == EarlyOverride {
+                        self.high_on = true;
+                        if self.low_on == false {
+                            self.selected_dose = dose;
+                            self.selected_mode = mode;
+                            controller.activate_frontal(dose, mode);
+                        }
+                    }
+                } else if dose == Low {
+                    if DESIGN_CHOICE == LowOverride {
+                        self.selected_dose = dose;
+                        self.selected_mode = mode;
+                        self.low_on = true;
+                        if self.high_on == false {
+                            controller.activate_frontal(dose, mode);
+                        } else {
+                            controller.deactivate_xray();
+                            controller.activate_frontal(dose, mode);
+                        }
+                    } else if DESIGN_CHOICE == HighOverride || DESIGN_CHOICE == EarlyOverride {
+                        self.low_on = true;
+                        if self.high_on == false {
+                            self.selected_dose = dose;
+                            self.selected_mode = mode;
+                            controller.activate_frontal(dose, mode);
+                        }
                     }
                 }
-                if projection == Biplane {
-                    self.p3_on = true;
-                    if self.p1_on == false && self.p2_on == true {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Frontal, dose, mode);
-                    } else if self.p1_on == true && self.p2_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Lateral, dose, mode);
-                    } else if self.p1_on == false && self.p2_on == false {
-                        controller.deactivate_xray();
-                        controller.activate_xray(Biplane, dose, mode);
-                    }
-                }
-                self.selected = projection;
-                self.selected_dose = dose;
-                self.selected_mode = mode;
             }
-
             Request::Stop {
                 projection,
                 dose,
                 mode, } => {
-                if projection == Frontal {
-                    self.p1_on = false;
-                } else if projection == Lateral {
-                    self.p2_on = false;
-                } else if projection == Biplane {
-                    self.p3_on = false;
-                }
-                if self.p5_on == false && self.p6_on == false {
-                    controller.deactivate_xray();
-                }
-                if projection != Biplane && self.p3_on == true {
-                    controller.activate_xray(Biplane, dose, mode);
-                }
-                if projection != Frontal && self.p1_on == true && self.p3_on == false {
-                    self.selected = Frontal;
-                    controller.activate_xray(Frontal, dose, mode);
-                }
-                if projection != Lateral && self.p2_on == true && self.p3_on == false {
-                    self.selected = Lateral;
-                    controller.activate_xray(Lateral, dose, mode);
-                }
-            }
-
-            Request::ToggleSelectedProjection => {
-                if self.p1_on == false && self.p2_on == false && self.p3_on == false {
-                    self.selected = match self.selected {
-                        Frontal => Lateral,
-                        Lateral => Biplane,
-                        Biplane => Frontal,
+                if dose == High {
+                    self.high_on = false;
+                    if self.low_on {
+                        controller.deactivate_xray();
+                        controller.activate_frontal(Low, Video);
+                    } else {
+                        controller.deactivate_xray();
                     }
-                };
-                info!("Selected: {:?}", self.selected);
-            }
-
-            Request::StartSelectedProjection { dose, mode } => {
-                if mode == Video {
-                    self.p5_on = true;
-                } else if mode == Image {
-                    self.p6_on = true;
-                }
-                controller.deactivate_xray();
-                controller.activate_xray(self.selected, dose, mode)
-            }
-
-            Request::StopSelectedProjection { dose, mode } => {
-                if mode == Video {
-                    self.p5_on = false;
-                } else if mode == Image {
-                    self.p6_on = false;
-                }
-
-                if self.p1_on == true || self.p2_on == true || self.p3_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, Low, Video);
-                } else if self.p1_on == false || self.p2_on == false || self.p3_on == false {
-                    controller.deactivate_xray();
-                    // controller.activate_xray(self.selected, dose, mode);
-                }
-
-                if self.p5_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, High, Video);
-                }
-                if self.p6_on == true {
-                    controller.deactivate_xray();
-                    controller.activate_xray(self.selected, High, Image);
+                } else {
+                    self.low_on = false;
+                    if self.high_on {
+                        controller.deactivate_xray();
+                        controller.activate_frontal(High, Video);
+                    } else {
+                        controller.deactivate_xray();
+                    }
                 }
             }
+            _ => {}
         }
     }
 }
